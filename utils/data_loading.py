08d1c43c61d29678addfa88df9abbb101eff4ce5
omrim123 <omrimishriki@campus.technion.ac.il>
2025-06-27 18:19:43 +0300

    added unet architecture train, evaluate, dataloading functions need to be addapted for our puprposes

--- utils/data_loading.py
+++ utils/data_loading.py
@@ -1,3 +1,57 @@
+import os
+from pathlib import Path
+from torch.utils.data import Dataset
+import numpy as np
+from PIL import Image
+
+class ISIC2018Task2(Dataset):
+    # The five attribute suffixes in the ground‐truth folder:
+    ATTRIBUTES = [
+        "pigment_network",
+        "negative_network",
+        "streaks",
+        "milia_like_cysts",
+        "globules",
+    ]
+
+    def __init__(self, images_dir, masks_dir, transform=None):
+        """
+        images_dir: Path to folder with ISIC_XXXXX.jpg
+        masks_dir:  Path to folder with ISIC_XXXXX_<attr>.png for each attr
+        """
+        # TODO: resize to 256x256
+        self.images_dir = Path(images_dir)
+        self.masks_dir  = Path(masks_dir)
+        self.ids        = [p.stem for p in self.images_dir.glob("ISIC_*.jpg")]
+        self.transform  = transform
+
+    def __len__(self):
+        return len(self.ids)
+
+    def __getitem__(self, idx): 
+        img_id = self.ids[idx]
+        # --- load the RGB image ---
+        img_path = self.images_dir / f"{img_id}.jpg"
+        img = np.array(Image.open(img_path).convert("RGB"))
+
+        # --- load & stack the 5 attribute masks ---
+        masks = []
+        for attr in self.ATTRIBUTES:
+            m_path = self.masks_dir / f"{img_id}_{atr}.png"
+            m     = np.array(Image.open(m_path).convert("L"), dtype=np.uint8)
+            # ensure binary 0/1
+            m     = (m > 127).astype(np.uint8)
+            masks.append(m)
+        # masks: list of (H,W) → stack → (H,W,5)
+        mask = np.stack(masks, axis=-1)
+
+        # --- apply any joint transforms (if given) ---
+        if self.transform:
+            img, mask = self.transform((img, mask))
+
+        return img, mask
+
+"""
 import logging
 import numpy as np
 import torch
@@ -115,3 +169,6 @@ class BasicDataset(Dataset):
 class CarvanaDataset(BasicDataset):
     def __init__(self, images_dir, mask_dir, scale=1):
         super().__init__(images_dir, mask_dir, scale, mask_suffix='_mask')
+
+"""
+
